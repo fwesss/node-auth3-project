c70d222c5f639f0802ececba99b91568905e352a
Westley Feller <feller.westley@gmail.com>
2020-02-20 12:20:57 -0800

    Retrieve only users in same department as logged in user

--- src/auth/middleware/checkAuth.ts
+++ src/auth/middleware/checkAuth.ts
@@ -5,30 +5,33 @@ import { UnauthorizedError } from '../../server/middleware/errorHandler'
 import { ValidationError } from '../../utils/validator'
 import secret from '../../secrets'
 
-interface AuthorizedRequest extends Request {
-  decodedJwt: string | object
+type DecodedJwt = {
+  subject: number
+  username: string
+  department: string
+  iat: number
+  exp: number
 }
 
-const instanceOfAuthorizedRequest = (
-  object: Request
-): object is AuthorizedRequest => 'decodedJwt' in object
+export interface AuthorizedRequest extends Request {
+  decodedJwt: DecodedJwt
+}
 
 const checkAuth = (
-  req: Request | AuthorizedRequest,
+  req: AuthorizedRequest,
   _res: Response,
   next: NextFunction
 ): void => {
   const token = req.headers.authorization
 
-  if (instanceOfAuthorizedRequest(req)) {
+  if (req.decodedJwt) {
     next()
   } else if (token) {
     jwt.verify(token, secret, (error, decodedJwt) => {
       if (error) {
         next(new UnauthorizedError({ message: 'You shall not pass!' }))
-      } else if (instanceOfAuthorizedRequest(req)) {
-        req.decodedJwt = decodedJwt
       } else {
+        req.decodedJwt = decodedJwt as DecodedJwt
         next()
       }
     })
